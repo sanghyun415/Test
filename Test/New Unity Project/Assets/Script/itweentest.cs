ecaf2cf1d3025c262147907992c3032a901e7be1
sanghyun <sanghyun222@naver.com>
2017-04-03 16:25:20 +0900

    유니티 테스트폴더

---
+++ Test/New Unity Project/Assets/Script/itweentest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itweentest : MonoBehaviour {
+    public GameObject ttt;
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(ttt, iTween.Hash("path", iTweenPath.GetPath("PosMove1"), "time", 5.5f, "orienttopath", false, "easetype", iTween.EaseType.linear, "oncomplete", "TweenStop"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void TweenStop()
+    {
+        Debug.Log("ttttttt");
+    }
+}
